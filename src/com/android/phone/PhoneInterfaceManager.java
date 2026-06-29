c43b99d5238aeed716e05982fdd2581bc94aa757
SagarMakhar <makharsagar@gmail.com>
2026-06-30 02:44:19 +0530

    Revert "Block overriding satellite carrier config keys on user builds"
    This reverts commit 20949e75b704f9533923cc55e7b5136e66d926cd.
    
    Fixes:
    android.net.vcn.cts.VcnManagerTest#testEnterSafeModeImmediately_safeModeEnabled
    android.net.vcn.cts.VcnManagerTest#testEnterSafeModeImmediately_safeModeDisabled
    android.net.vcn.cts.VcnManagerTest#testVcnSafeModeOnTestNetwork_overrideTimeout
    
    Change-Id: Ifce6bbe80257d0f407677e9e7135e3ba6824e6b1

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -12854,6 +12854,7 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
     public String getModemService() {
         String result;
         Log.d(LOG_TAG, "getModemService");
+        TelephonyPermissions.enforceShellOnly(Binder.getCallingUid(), "getModemService");
         TelephonyPermissions
                 .enforceCallingOrSelfReadPrivilegedPhoneStatePermissionOrCarrierPrivilege(
                         mApp, SubscriptionManager.INVALID_SUBSCRIPTION_ID,
